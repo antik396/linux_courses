4ed44436a74d7d96548db585d9a2b2beaab6f7f0
Anton <3968204@gmail.com>
2016-11-13 20:12:58 +0300

    add ; to the and of each func

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -3,8 +3,8 @@
 
 for fb in {1..100}
 do
-{ (( fb % 3 == 0 )) && (( fb % 5 == 0 )) && echo 'FizzBuzz' } ||
-{ (( fb % 3 == 0 )) && echo 'Fizz' } ||
-{ (( fb % 5 == 0 )) && echo 'Buzz' } ||
+{ (( fb % 3 == 0 )) && (( fb % 5 == 0 )) && echo 'FizzBuzz'; } ||
+{ (( fb % 3 == 0 )) && echo 'Fizz'; } ||
+{ (( fb % 5 == 0 )) && echo 'Buzz'; } ||
 echo $fb
 done
